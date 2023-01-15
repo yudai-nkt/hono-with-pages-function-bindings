6b39df837e9a5ca4680f4a9f772d41e956554b12
Yudai NAKATA <yudai.nkt@gmail.com>
2023-01-15 14:17:17 +0900

    refactor: remove React and Vite

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { type UserConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
-export default {
-  plugins: [react()],
-} satisfies UserConfig
